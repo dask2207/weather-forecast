afb1a898b282bc55fdab512202852a8d6dda4909
krishna das <dask2207@gmail.com>
2024-03-09 17:42:25 +0530

    add search input

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,13 @@
+import Inputs from "./Inputs";
+import TimeAndLocation from "./TimeAndLocation";
 import TopButton from "./TopButton";
 
 const Body = () => {
     return (
         <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
             <TopButton />
+            <Inputs />
+            <TimeAndLocation />
         </div>
     )
 }
---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const TimeAndLocation = () => {
+  return (
+    <div>
+        <div className='flex justify-center items-center my-6'>
+            <p className='text-white text-xl font-extralight'>
+                Saturday, 09 March 2024 | Local time: 14:54 PM
+            </p>
+        </div>
+
+        <div className='flex items-center justify-center my-3'>
+            <p className='text-white text-3xl font-medium'>
+                Berlin, DE
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default TimeAndLocation
